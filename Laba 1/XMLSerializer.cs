07158359924c831dabdc795cb7eafa6e7e802585
willysteams <willysteams@gmail.com>
2022-10-10 14:47:04 +0300

    adding xml serializer

--- Laba 1/XMLSerializer.cs
+++ Laba 1/XMLSerializer.cs
@@ -10,6 +10,21 @@ namespace Laba_1
     /// </summary>
     class TraceResultXMLSerializer : ISerializer
     {
-        
+        /// <summary>
+        /// Serializes the given TraceResult
+        /// </summary>
+        /// <param name="tr">TraceResult</param>
+        /// <returns>XML serialization result as a byte array</returns>
+        public byte[] Serialize(TraceResult tr)
+        {
+            
+            DataContractSerializer ser = new DataContractSerializer(typeof(TraceResult));
+            MemoryStream ms = new MemoryStream();
+            XmlWriterSettings writerSettings = new XmlWriterSettings() { Indent = true, IndentChars = "\t", NewLineOnAttributes = true };
+            using (var xmlWriter = XmlWriter.Create(ms, writerSettings))
+                ser.WriteObject(xmlWriter, tr);
+            
+            return ms.ToArray();
+        }
     }
 }
